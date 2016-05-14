267a284938c934b05a8afa478782ac4ae69415ac
Mateusz Harciarek <mh359520@students.mimuw.edu.pl>
2016-05-15 00:42:41 +0200

    commit 1

--- src/middle_ages.c
+++ src/middle_ages.c
@@ -7,23 +7,23 @@
 int main() {
 
     start_game();
-
+    printf("pierwsza zmiana w programie");
     command *new_command;
-    while (1) {
-        new_command = parse_command();
-
-        if (strcmp(new_command->name, "INIT") == 0) {
-            init(new_command->data[0],
-                 new_command->data[1],
-                 new_command->data[2],
-                 new_command->data[3],
-                 new_command->data[4],
-                 new_command->data[5],
-                 new_command->data[6]);
-        }
-
-        print_topleft();
-    }
+//    while (1) {
+//        new_command = parse_command();
+//
+//        if (strcmp(new_command->name, "INIT") == 0) {
+//            init(new_command->data[0],
+//                 new_command->data[1],
+//                 new_command->data[2],
+//                 new_command->data[3],
+//                 new_command->data[4],
+//                 new_command->data[5],
+//                 new_command->data[6]);
+//        }
+//
+//        print_topleft();
+//    }
 
     end_game();
 
